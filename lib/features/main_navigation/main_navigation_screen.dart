140002e516a5ca79327e8cad988e22916bb9f56f
임우현 <shinedngus225@gmail.com>
2024-04-09 15:10:09 +0900

    CupertinoTabBar

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -65,18 +66,30 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _screens[_currentIndex],
-      bottomNavigationBar: NavigationBar(
-        backgroundColor: Colors.black,
-        indicatorColor: Colors.white10,
-        indicatorShape: const CircleBorder(eccentricity: 1),
-        labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
-        onDestinationSelected: _onTap,
-        selectedIndex: _currentIndex,
-        animationDuration: const Duration(milliseconds: 500),
-        destinations: _destinations,
-      ),
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(items: const [
+        BottomNavigationBarItem(
+          backgroundColor: Colors.black,
+          icon: Icon(CupertinoIcons.house),
+          label: "Home",
+        ),
+        BottomNavigationBarItem(
+          backgroundColor: Colors.black,
+          icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+          label: "Discover",
+        ),
+        BottomNavigationBarItem(
+          backgroundColor: Colors.black,
+          icon: FaIcon(FontAwesomeIcons.comment),
+          label: "Inbox",
+        ),
+        BottomNavigationBarItem(
+          backgroundColor: Colors.black,
+          icon: FaIcon(FontAwesomeIcons.user),
+          label: "Profile",
+        ),
+      ]),
+      tabBuilder: (context, index) => _screens[index],
     );
   }
 }
@@ -128,3 +141,80 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
 //     );
 //   }
 // }
+
+/////////////////////////
+class Material3 extends StatefulWidget {
+  const Material3({super.key});
+
+  @override
+  State<Material3> createState() => _Material3State();
+}
+
+class _Material3State extends State<Material3> {
+  int _currentIndex = 0;
+
+  final List<Widget> _destinations = [
+    const NavigationDestination(
+      icon: FaIcon(
+        FontAwesomeIcons.house,
+        color: Colors.white,
+      ),
+      label: "House",
+    ),
+    const NavigationDestination(
+      icon: FaIcon(
+        FontAwesomeIcons.magnifyingGlass,
+        color: Colors.white,
+      ),
+      label: "Discover",
+    ),
+    const NavigationDestination(
+      icon: FaIcon(
+        FontAwesomeIcons.comment,
+        color: Colors.white,
+      ),
+      label: "Inbox",
+    ),
+    const NavigationDestination(
+      icon: FaIcon(
+        FontAwesomeIcons.user,
+        color: Colors.white,
+      ),
+      label: "Profile",
+    ),
+  ];
+
+  final List<Widget> _screens = [
+    const Center(
+      child: Text('asdasd'),
+    ),
+    const Center(
+      child: Text('asda1sd'),
+    ),
+    const Center(
+      child: Text('asda2sd'),
+    ),
+    const Center(
+      child: Text('asd3asd'),
+    )
+  ];
+  void _onTap(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationBar(
+      backgroundColor: Colors.black,
+      indicatorColor: Colors.white10,
+      indicatorShape: const CircleBorder(eccentricity: 1),
+      labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+      onDestinationSelected: _onTap,
+      selectedIndex: _currentIndex,
+      animationDuration: const Duration(milliseconds: 500),
+      destinations: _destinations,
+    );
+  }
+}
