823cb5324ac14e3255f344c7ddc91cc4d482d331
임우현 <shinedngus225@gmail.com>
2024-04-09 13:49:57 +0900

    BottomNavigationBar

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class MainNavigationScreen extends StatelessWidget {
+class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
+
+  @override
+  State<MainNavigationScreen> createState() => _MainNavigationScreenState();
+}
+
+class _MainNavigationScreenState extends State<MainNavigationScreen> {
+  int _currentIndex = 0;
+
+  List<Widget> screens = [
+    const Center(
+      child: Text('Home'),
+    ),
+    const Center(
+      child: Text('Discover'),
+    ),
+    const Center(
+      child: Text('Inbox'),
+    ),
+    const Center(
+      child: Text('Profile'),
+    )
+  ];
+
+  void _onTap(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: screens[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
+        onTap: _onTap,
+        currentIndex: _currentIndex,
         selectedItemColor: Colors.white,
         unselectedItemColor: Colors.white54,
         items: const [
@@ -17,14 +48,17 @@ class MainNavigationScreen extends StatelessWidget {
             label: "Home",
           ),
           BottomNavigationBarItem(
+            backgroundColor: Colors.black,
             icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
             label: "Discover",
           ),
           BottomNavigationBarItem(
+            backgroundColor: Colors.black,
             icon: FaIcon(FontAwesomeIcons.comment),
             label: "Inbox",
           ),
           BottomNavigationBarItem(
+            backgroundColor: Colors.black,
             icon: FaIcon(FontAwesomeIcons.user),
             label: "Profile",
           ),
