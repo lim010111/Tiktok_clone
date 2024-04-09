63799644271c0daa2e902d2173a9feb3881bad0c
임우현 <shinedngus225@gmail.com>
2024-04-09 16:31:10 +0900

    CustomWidget(기본기)

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/features/main_navigation/widgets/nav_tab.dart';
 
 class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
@@ -66,155 +66,56 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoTabScaffold(
-      tabBar: CupertinoTabBar(items: const [
-        BottomNavigationBarItem(
-          backgroundColor: Colors.black,
-          icon: Icon(CupertinoIcons.house),
-          label: "Home",
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              NavTap(
+                isSelected: _currentIndex == 0,
+                name: "Home",
+                icon: FontAwesomeIcons.house,
+                onTap: () => _onTap(0),
+              ),
+              NavTap(
+                isSelected: _currentIndex == 1,
+                name: "Discover",
+                icon: FontAwesomeIcons.magnifyingGlass,
+                onTap: () => _onTap(1),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 10),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 10,
+                  ),
+                  child: const FaIcon(FontAwesomeIcons.plus),
+                ),
+              ),
+              NavTap(
+                isSelected: _currentIndex == 3,
+                name: 'Inbox',
+                icon: FontAwesomeIcons.comment,
+                onTap: () => _onTap(3),
+              ),
+              NavTap(
+                isSelected: _currentIndex == 4,
+                name: 'Profile',
+                icon: FontAwesomeIcons.user,
+                onTap: () => _onTap(4),
+              )
+            ],
+          ),
         ),
-        BottomNavigationBarItem(
-          backgroundColor: Colors.black,
-          icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
-          label: "Discover",
-        ),
-        BottomNavigationBarItem(
-          backgroundColor: Colors.black,
-          icon: FaIcon(FontAwesomeIcons.comment),
-          label: "Inbox",
-        ),
-        BottomNavigationBarItem(
-          backgroundColor: Colors.black,
-          icon: FaIcon(FontAwesomeIcons.user),
-          label: "Profile",
-        ),
-      ]),
-      tabBuilder: (context, index) => _screens[index],
-    );
-  }
-}
-
-// class MyWidget extends StatefulWidget {
-//   const MyWidget({super.key});
-
-//   @override
-//   State<MyWidget> createState() => _MyWidgetState();
-// }
-
-// class _MyWidgetState extends State<MyWidget> {
-//   int _currentIndex = 0;
-//   void _onTap(int index) {
-//     setState(() {
-//       _currentIndex = index;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return BottomNavigationBar(
-//       onTap: _onTap,
-//       currentIndex: _currentIndex,
-//       selectedItemColor: Colors.white,
-//       unselectedItemColor: Colors.white54,
-//       items: const [
-//         BottomNavigationBarItem(
-//           backgroundColor: Colors.black,
-//           icon: FaIcon(FontAwesomeIcons.house),
-//           label: "Home",
-//         ),
-//         BottomNavigationBarItem(
-//           backgroundColor: Colors.black,
-//           icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
-//           label: "Discover",
-//         ),
-//         BottomNavigationBarItem(
-//           backgroundColor: Colors.black,
-//           icon: FaIcon(FontAwesomeIcons.comment),
-//           label: "Inbox",
-//         ),
-//         BottomNavigationBarItem(
-//           backgroundColor: Colors.black,
-//           icon: FaIcon(FontAwesomeIcons.user),
-//           label: "Profile",
-//         ),
-//       ],
-//     );
-//   }
-// }
-
-/////////////////////////
-class Material3 extends StatefulWidget {
-  const Material3({super.key});
-
-  @override
-  State<Material3> createState() => _Material3State();
-}
-
-class _Material3State extends State<Material3> {
-  int _currentIndex = 0;
-
-  final List<Widget> _destinations = [
-    const NavigationDestination(
-      icon: FaIcon(
-        FontAwesomeIcons.house,
-        color: Colors.white,
       ),
-      label: "House",
-    ),
-    const NavigationDestination(
-      icon: FaIcon(
-        FontAwesomeIcons.magnifyingGlass,
-        color: Colors.white,
-      ),
-      label: "Discover",
-    ),
-    const NavigationDestination(
-      icon: FaIcon(
-        FontAwesomeIcons.comment,
-        color: Colors.white,
-      ),
-      label: "Inbox",
-    ),
-    const NavigationDestination(
-      icon: FaIcon(
-        FontAwesomeIcons.user,
-        color: Colors.white,
-      ),
-      label: "Profile",
-    ),
-  ];
-
-  final List<Widget> _screens = [
-    const Center(
-      child: Text('asdasd'),
-    ),
-    const Center(
-      child: Text('asda1sd'),
-    ),
-    const Center(
-      child: Text('asda2sd'),
-    ),
-    const Center(
-      child: Text('asd3asd'),
-    )
-  ];
-  void _onTap(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return NavigationBar(
-      backgroundColor: Colors.black,
-      indicatorColor: Colors.white10,
-      indicatorShape: const CircleBorder(eccentricity: 1),
-      labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
-      onDestinationSelected: _onTap,
-      selectedIndex: _currentIndex,
-      animationDuration: const Duration(milliseconds: 500),
-      destinations: _destinations,
     );
   }
 }
---
+++ lib/features/main_navigation/widgets/nav_tab.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class NavTap extends StatelessWidget {
+  const NavTap({
+    super.key,
+    required this.name,
+    required this.icon,
+    required this.isSelected,
+    required this.onTap,
+  });
+
+  final String name;
+  final IconData icon;
+  final bool isSelected;
+
+  final Function onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GestureDetector(
+        onTap: () => onTap(),
+        child: Container(
+          color: Colors.red,
+          child: AnimatedOpacity(
+            duration: const Duration(milliseconds: 200),
+            opacity: isSelected ? 1 : 0.60,
+            child: Column(
+              children: [
+                FaIcon(
+                  icon,
+                  color: Colors.white,
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  name,
+                  style: const TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
