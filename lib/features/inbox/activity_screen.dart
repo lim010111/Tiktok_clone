67dbfb6d9c3fc6c27b92193f715584c7da32442b
임우현 <shinedngus225@gmail.com>
2024-04-17 10:55:24 +0900

    Dismissible

--- lib/features/inbox/activity_screen.dart
+++ lib/features/inbox/activity_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -13,59 +14,77 @@ class ActivityScreen extends StatelessWidget {
         title: const Text('All activity'),
       ),
       body: ListView(
-        padding: EdgeInsets.symmetric(horizontal: 12.w),
         children: [
-          Text(
-            'New',
-            style: TextStyle(
-              color: Colors.grey.shade600,
-              fontSize: 14,
-              fontWeight: FontWeight.w500,
+          Padding(
+            padding: EdgeInsets.only(left: 12.w, bottom: 10.h),
+            child: Text(
+              'New',
+              style: TextStyle(
+                color: Colors.grey.shade600,
+                fontSize: 14,
+                fontWeight: FontWeight.w500,
+              ),
             ),
           ),
-          ListTile(
-            contentPadding: EdgeInsets.zero,
-            leading: Container(
-              width: 52,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.white,
-                border: Border.all(
-                  width: 0.7,
-                  color: Colors.grey.shade400,
-                ),
-              ),
-              child: const Center(
-                child: FaIcon(FontAwesomeIcons.bell),
+          Dismissible(
+            key: const Key(''),
+            background: Container(
+              decoration: const BoxDecoration(color: Colors.green),
+              alignment: Alignment.centerLeft,
+              child: const FaIcon(
+                FontAwesomeIcons.check,
               ),
             ),
-            title: RichText(
-              text: TextSpan(
-                text: 'Account Updates: ',
-                style: const TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.w600,
-                  fontSize: 16,
-                ),
-                children: [
-                  const TextSpan(
-                    text: 'Upload longer videos',
-                    style: TextStyle(fontWeight: FontWeight.normal),
+            secondaryBackground: Container(
+              decoration: const BoxDecoration(color: Colors.red),
+              alignment: Alignment.centerRight,
+              child: const FaIcon(
+                FontAwesomeIcons.trashCan,
+              ),
+            ),
+            child: ListTile(
+              leading: Container(
+                width: 52,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white,
+                  border: Border.all(
+                    width: 0.7,
+                    color: Colors.grey.shade400,
                   ),
-                  TextSpan(
-                    text: ' 1h',
-                    style: TextStyle(
-                      color: Colors.grey.shade400,
+                ),
+                child: const Center(
+                  child: FaIcon(FontAwesomeIcons.bell),
+                ),
+              ),
+              title: RichText(
+                text: TextSpan(
+                  text: 'Account Updates: ',
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 16,
+                  ),
+                  children: [
+                    const TextSpan(
+                      text: 'Upload longer videos',
+                      style: TextStyle(fontWeight: FontWeight.normal),
                     ),
-                  )
-                ],
+                    TextSpan(
+                      text: ' 1h',
+                      style: TextStyle(
+                        color: Colors.grey.shade400,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              trailing: const FaIcon(
+                FontAwesomeIcons.chevronRight,
+                size: 18,
               ),
             ),
-            trailing: const FaIcon(
-              FontAwesomeIcons.chevronRight,
-              size: 18,
-            ),
-          )
+          ),
         ],
       ),
     );
